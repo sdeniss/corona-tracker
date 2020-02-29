0af58e3d1fe030d888ac33244bc8898a276dba6f
denis <denis@veego.io>
2020-02-28 23:41:58 -0500

    second

---
+++ app.py
@@ -0,0 +1,49 @@
+import json
+from datetime import datetime
+
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('main.html')
+
+
+def _textulize_visit_time(point):
+    start = point['t_start']
+    end = point['t_end']
+    dt_start = datetime.fromisoformat(start)
+    dt_end = datetime.fromisoformat(end)
+    return '%s בין השעות %s-%s' % (dt_start.strftime('%d/%m'), dt_start.strftime('%H:%M'), dt_end.strftime('%H:%M'))
+
+
+@app.route('/api/dangerZone')
+def api_dz():
+    visit_dict = {}
+    with open('positions.json') as f:
+        points = json.load(f)
+        for point in points:
+            pos = tuple(point['position'])
+            if pos not in visit_dict:
+                visit_dict[pos] = [point]
+            else:
+                visit_dict[pos].append(point)
+
+    result = []
+
+    for points in visit_dict.values():
+        point = dict(points[0])
+        point['description'] += '<br><br><b>שעות ביקור:</b><br>'
+
+        for p_ in points:
+            point['description'] += '<li>' + _textulize_visit_time(p_)
+
+        result.append(point)
+
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ static/js/main.js
@@ -0,0 +1,117 @@
+var ngApp = angular.module('app', ['relativeDate']);
+
+ngApp.controller('baseController', function($scope, $http){
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic2RlbmlzcyIsImEiOiJjazc2eWhrc3AwMjdnM2ZwOTh5emc0YTk3In0.GLdzpbgoNIp_CfhfRpkT0g';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11', //streets-v11 or light-v10
+        center: [35.0258957,31.5435208],
+        zoom: 8
+    });
+
+    var data = [];
+    var popup = undefined;
+
+    [[34.8022113, 32.1078663]].forEach(function(point) {
+
+    });
+
+    map.loadImage('/static/img/biohazard.png', function(error, image) {
+       if (error) throw error;
+       if (!map.hasImage('biohazard')) map.addImage('biohazard', image);
+    });
+
+
+    map.on('load', function() {
+
+        map.setLayoutProperty('country-label', 'text-field', [
+        'get',
+        'name_en'
+        ]);
+        map.addSource('places', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': data
+            }
+        });
+        // Add a layer showing the places.
+        map.addLayer({
+            'id': 'places',
+            'type': 'symbol',
+            'source': 'places',
+            'layout': {
+                'icon-image': 'biohazard',
+                'icon-allow-overlap': true,
+                'icon-size': 0.5
+            }
+        });
+
+        // When a click event occurs on a feature in the places layer, open a popup at the
+        // location of the feature, with description HTML from its properties.
+        map.on('click', 'places', function(e) {
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            var description = e.features[0].properties.description;
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            new mapboxgl.Popup({className: 'mapPopup'})
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+
+            map.flyTo({
+                center: coordinates,
+                zoom: 16
+            });
+        });
+
+        // Change the cursor to a pointer when the mouse is over the places layer.
+        map.on('mouseenter', 'places', function(e) {
+            map.getCanvas().style.cursor = 'pointer';
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            var description = e.features[0].properties.description;
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            popup = new mapboxgl.Popup({className: 'mapPopup hoverPopup'})
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        });
+
+        // Change it back to a pointer when it leaves.
+        map.on('mouseleave', 'places', function() {
+            map.getCanvas().style.cursor = '';
+            popup.remove();
+        });
+    });
+    $http.get('/api/dangerZone').then(function(response) {
+        response.data.forEach(function(point) {
+            data.push({
+                        'type': 'Feature',
+                        'properties': {
+                            'description':
+                                '<strong>' + point.label + '</strong>' +
+                                '<p>' + point.description + '</p>' +
+                                '<a href="' + point.link + '" target="_blank" title="Opens in a new window">עוד</a> ',
+                            'icon': 'theatre'
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': point.position
+                        }
+                    });
+        });
+    });
+});
