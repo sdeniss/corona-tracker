c9becb5a5079c64064f7f548138fecc4f4639758
Ofer Bartal <blueofer@gmail.com>
2020-03-06 00:58:21 +0200

    Data script changes

--- csv2json.py
+++ csv2json.py
@@ -1,5 +1,5 @@
 import csv
-
+import json
 
 def read_csv(filename):
     result = []
@@ -7,17 +7,19 @@ def read_csv(filename):
         reader = csv.DictReader(f)
         for row in reader:
             place_name = row['label'].strip()
-            location = row['position']
             location = row['position'].split(',')
-            location = float(location[1].strip()), float(location[0].strip())
+            lat = float(location[0].strip())
+            lon = float(location[1].strip())
+            # TODO: Switch order to lat, lon in next version ;)
+            location = lon, lat
             t_start = row['start time'].strip()
             t_end = row['end time'].strip()
-            descriptioin = row['description'].strip()
+            description = row['description'].strip()
             link = row['link'].strip()
             # TODO: Set radius in CSV file, not here
             result.append({"position": location,
                             "label": place_name,
-                            "description": descriptioin,
+                            "description": description,
                             "t_start": t_start,
                             "t_end": t_end,
                             "radius": 15,
@@ -25,3 +27,5 @@ def read_csv(filename):
 
     return result
 
+with open('data/data.json', 'w', encoding='utf-8') as f:
+    json.dump(read_csv('data/data.csv'), f, ensure_ascii=False, indent=4)
