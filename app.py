4916046c8df1c6d5a8831139eeb8f79f5d418281
denis <denis@veego.io>
2020-03-11 02:54:40 +0200

    Data is loaded from json now

--- app.py
+++ app.py
@@ -10,62 +10,21 @@ from csv2json import read_csv
 app = Flask(__name__)
 
 
-def load():
-    data = []
-    dir = join(dirname(__file__), 'data')
-    for fname in listdir(dir):
-        data += read_csv(join(dir, fname))
-    with open('data.json', 'w') as f:
-        json.dump(data, f)
-
-
-load()
-
-
 @app.route('/')
 def hello_world():
     return render_template('main.html')
 
 
-def _textulize_visit_time(point):
-    start = point['t_start']
-    end = point['t_end']
-    dt_start = datetime.fromisoformat(start)
-    dt_end = datetime.fromisoformat(end)
-    return '%s בין השעות %s-%s' % (dt_start.strftime('%d/%m'), dt_start.strftime('%H:%M'), dt_end.strftime('%H:%M'))
-
-
 @app.route('/api/dangerZone')
 def api_dz():
-    visit_dict = {}
-    with open('data.json') as f:
-        points = json.load(f)
-        for point in points:
-            pos = tuple(point['position'])
-            if pos not in visit_dict:
-                visit_dict[pos] = [point]
-            else:
-                visit_dict[pos].append(point)
-
-    result = []
-
-    for points in visit_dict.values():
-        point = dict(points[0])
-        point['description'] += '<br><br><b>שעות ביקור:</b><br>'
-
-        for p_ in points:
-            point['description'] += '<li>' + _textulize_visit_time(p_)
-
-        result.append(point)
-
-    return jsonify(result)
+    with open('merged_points.json') as f:
+        return jsonify(json.load(f))
 
 
 @app.route('/api/v1/dangerZone/<position>')
 def public_api_dz(position):
-    with open('data.json') as f:
-        points = json.load(f)
-    return jsonify(list(points))
+    with open('points.json') as f:
+        return jsonify(json.load(f))
 
 
 if __name__ == '__main__':
